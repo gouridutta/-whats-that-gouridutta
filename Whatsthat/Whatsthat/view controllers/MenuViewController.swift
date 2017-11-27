1e3ab26a313ea1970d6804eceeae32893d53b458
gouridutta <gouridutta@gwu.edu>
2017-11-27 00:07:55 -0500

    Project Structure

---
+++ Whatsthat/Whatsthat/view controllers/MenuViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MenuViewController.swift
+//  Whatsthat
+//
+//  Created by Gouri Dutta on 11/26/17.
+//  Copyright © 2017 Gouri Dutta. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        print("Camera Pressed")
+    }
+    
+    @IBAction func favoritesButtonPressed(_ sender: Any) {
+        print("Favorite Pressed")
+    }
+    
+}
+
